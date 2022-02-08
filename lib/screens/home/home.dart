0ce1dc30bb6218965051b9457a108171a384e2f4
Grace Cai <grace.cai7@gmail.com>
2022-02-08 11:13:31 -0800

    deleted uneccesary files

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:ide_app/models/stat.dart';
+import 'package:ide_app/services/auth.dart';
+import 'package:ide_app/services/database.dart';
+import 'package:provider/provider.dart';
+
+class Home extends StatelessWidget {
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
