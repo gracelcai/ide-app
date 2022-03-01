e5ea244c99a252e4b4d49d8f323235a62373fdd7
Anjali Mathew <100025634@mvla.net>
2022-03-01 11:27:48 -0800

    new project page

--- lib/new_project.dart
+++ lib/new_project.dart
@@ -9,9 +9,60 @@ class NewProject extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("New Project")),
-      body: const Center(
-        child: Text('create a new Project'),
-      ),
+      body: const ProjectInfo(),
+    );
+  }
+}
+
+class ProjectInfo extends StatelessWidget {
+  const ProjectInfo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Project Title',
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Project Description',
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Project Goals',
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: ElevatedButton(
+            onPressed: () {
+              //also needs to somehow make a project that shows up in home page
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const MyHomePage()),
+              );
+            },
+            child: const Text('Create Project'),
+          ),
+        )
+      ],
     );
   }
 }
