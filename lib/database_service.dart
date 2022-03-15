cd5d7341b9771936521c4870c074de2f271cd874
Anjali Mathew <100025634@mvla.net>
2022-03-15 10:56:05 -0700

    database error

--- lib/database_service.dart
+++ lib/database_service.dart
@@ -49,35 +49,32 @@ class DatabaseService {
   //   return projects;
   // }
 
-  Future<DocumentReference<Object?>> getUserDoc() async {
-    User? user = AuthenticationService(_firebaseAuth).getUser();
-    QuerySnapshot querySnap =
-        await users.where('userid', isEqualTo: user!.uid).get();
-    QueryDocumentSnapshot doc = querySnap.docs[0];
-    DocumentReference docRef = doc.reference;
+  // Future<DocumentReference<Object?>> getUserDoc() async {
+  //   User? user = AuthenticationService(_firebaseAuth).getUser();
+  //   QuerySnapshot querySnap =
+  //       await users.where('userid', isEqualTo: user!.uid).get();
+  //   QueryDocumentSnapshot doc = querySnap.docs[0];
+  //   DocumentReference docRef = doc.reference;
 
-    return docRef;
-  }
+  //   return docRef;
+  // }
 
+  // String getUserDocId() {
+  //   Future<DocumentReference<Object?>> ref = getUserDoc();
+  //   print(ref.toString());
+  //   return ref.toString();
+  // }
 
-  String getUserDocId() {
-    DocumentReference ref = getUserDoc() as DocumentReference;
-    return (ref.id);
-  }
-
-  Future<void> addTask(String task) {
-    await FirebaseFirestore
-    .instance
-    .collection('orders')
-    .doc(getUserDocId())
-    .collection(
-        "user_orders")
-    .add({
-        'task': task,
-        'complete': false,
-        });
-    print("Added task");
-    return task;
-  }
-
+  // Future<void> addTask(String task) async {
+  //   Future<void> newTask = FirebaseFirestore.instance
+  //       .collection('users')
+  //       .doc(getUserDocId())
+  //       .collection("tasks")
+  //       .add({
+  //     'task': task,
+  //     'complete': false,
+  //   });
+  //   print("Added task");
+  //   return newTask;
+  // }
 }
